c4fc8e1276d9b2dbf3fb7976c34bea830e89c04e
Anuradha <www.niroshonmora@gmail.com>
2016-04-30 15:48:42 +0530

    created the election table

---
+++ database/migrations/2016_04_30_100546_create_Election_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateElectionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('election', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('admin_id')->unsigned();
+            $table->string('name');
+            $table->text('details')->nullable();
+            $table->date('start_date');
+            $table->time('start_time');
+            $table->date('end_date');
+            $table->time('end_time');
+            $table->timestamps();
+
+            $table->foreign('admin_id')
+                ->references('id')
+                ->on('admin')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('election');
+    }
+}
