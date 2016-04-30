916cb1bfa526c06b23db2bdb21d10fabb2b026a1
Anuradha <www.niroshonmora@gmail.com>
2016-04-30 15:30:44 +0530

    Created few backend databases

---
+++ database/migrations/2016_04_30_074405_create_candidate_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidate', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('image_path');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('candidate');
+    }
+}
