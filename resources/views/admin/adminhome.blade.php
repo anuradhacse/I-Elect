a27cb9f2efba505e8778d829abf43697938bc87f
Anuradha <www.niroshonmora@gmail.com>
2016-05-24 19:00:48 +0530

    admin home edited

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -1,58 +1,7 @@
 @extends('layouts.app')
 
 @section('head')
-    <style>
-        .with-nav-tabs.panel-info .nav-tabs > li > a,
-        .with-nav-tabs.panel-info .nav-tabs > li > a:hover,
-        .with-nav-tabs.panel-info .nav-tabs > li > a:focus {
-            color: #31708f;
-        }
-
-        .with-nav-tabs.panel-info .nav-tabs > .open > a,
-        .with-nav-tabs.panel-info .nav-tabs > .open > a:hover,
-        .with-nav-tabs.panel-info .nav-tabs > .open > a:focus,
-        .with-nav-tabs.panel-info .nav-tabs > li > a:hover,
-        .with-nav-tabs.panel-info .nav-tabs > li > a:focus {
-            color: #31708f;
-            background-color: #bce8f1;
-            border-color: transparent;
-        }
-
-        .with-nav-tabs.panel-info .nav-tabs > li.active > a,
-        .with-nav-tabs.panel-info .nav-tabs > li.active > a:hover,
-        .with-nav-tabs.panel-info .nav-tabs > li.active > a:focus {
-            color: #31708f;
-            background-color: #fff;
-            border-color: #bce8f1;
-            border-bottom-color: transparent;
-        }
-
-        .with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu {
-            background-color: #d9edf7;
-            border-color: #bce8f1;
-        }
-
-        .with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > li > a {
-            color: #31708f;
-        }
-
-        .with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > li > a:hover,
-        .with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > li > a:focus {
-            background-color: #bce8f1;
-        }
-
-        .with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > .active > a,
-        .with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > .active > a:hover,
-        .with-nav-tabs.panel-info .nav-tabs > li.dropdown .dropdown-menu > .active > a:focus {
-            color: #fff;
-            background-color: #31708f;
-        }
-
-        .panel-heading {
-            height: 53px;
-        }
-
-    </style>
+    @include('partials.navcsspartial')
 @endsection
 
 @section('content')
@@ -113,7 +62,7 @@
                                 <button type="submit" class="btn btn-danger btn-mini">Delete</button>
                                 {!! Form::close() !!}</td>
 
-                            <td>delete</td>
+
                         </tr>
 
                     @endforeach
