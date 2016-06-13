270f4b1c55a2aa206c6bbbae1c2ce6e4f679fce0
Anuradha <www.niroshonmora@gmail.com>
2016-06-13 21:23:46 +0530

    adding voterpartial to finalize elections to show voters done

--- resources/views/partials/voterpartial.blade.php
+++ resources/views/partials/voterpartial.blade.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Anuradha
- * Date: 6/13/2016
- * Time: 9:21 PM
- */
+<table id="example" class="display" cellspacing="0" width="100%">
+    <thead>
+    <tr>
+        <th>Voter name</th>
+        <th>Email</th>
+        <th>Voted</th>
+        <th>Action</th>
+
+    </tr>
+    </thead>
+    <tfoot>
+    <tr>
+        <th>Voter name</th>
+        <th>Email</th>
+        <th>Voted</th>
+        <th>Action</th>
+
+    </tr>
+    </tfoot>
+    <tbody>
+
+    @foreach($voters as $voter)
+        <tr>
+            <td>{{$voter->name}}</td>
+            <td>{{\App\User::where('id',$voter->user_id)->first()->email}}</td>
+            @if($election->voters()->findOrFail($voter['id'])->pivot->candidate_id==null)
+                <td class="alert-danger"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>
+                    <span class="sr-only">Error:</span>
+                    Not Voted</td>
+            @else
+                <td class="alert-success"><span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+                    <span class="sr-only">Error:</span>
+                    Voted</td>
+            @endif
+            <td>{!! Form::open(['method'=>'DELETE','action' => ['VoterController@delete', $voter->id]])  !!}
+                {!! Form::hidden('election_id',$id) !!}
+                <button type="submit" class="btn btn-danger btn-mini">Delete</button>
+                {!! Form::close() !!}</td>
+
+
+        </tr>
+
+    @endforeach
+
+
+    </tbody>
+</table>
--- resources/views/voter/create.blade.php
+++ resources/views/voter/create.blade.php
@@ -46,53 +46,7 @@
 
                             <!-- dispaly voters who have already added-->
 
-                    <table id="example" class="display" cellspacing="0" width="100%">
-                        <thead>
-                        <tr>
-                            <th>Voter name</th>
-                            <th>Email</th>
-                            <th>Voted</th>
-                            <th>Action</th>
-
-                        </tr>
-                        </thead>
-                        <tfoot>
-                        <tr>
-                            <th>Voter name</th>
-                            <th>Email</th>
-                            <th>Voted</th>
-                            <th>Action</th>
-
-                        </tr>
-                        </tfoot>
-                        <tbody>
-
-                        @foreach($voters as $voter)
-                            <tr>
-                                <td>{{$voter->name}}</td>
-                                <td>{{\App\User::where('id',$voter->user_id)->first()->email}}</td>
-                                @if($election->voters()->findOrFail($voter['id'])->pivot->candidate_id==null)
-                                    <td class="alert-danger"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>
-                                        <span class="sr-only">Error:</span>
-                                        Not Voted</td>
-                                    @else
-                                     <td class="alert-success"><span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
-                                        <span class="sr-only">Error:</span>
-                                        Voted</td>
-                                    @endif
-                                <td>{!! Form::open(['method'=>'DELETE','action' => ['VoterController@delete', $voter->id]])  !!}
-                                    {!! Form::hidden('election_id',$id) !!}
-                                    <button type="submit" class="btn btn-danger btn-mini">Delete</button>
-                                    {!! Form::close() !!}</td>
-
-
-                            </tr>
-
-                        @endforeach
-
-
-                        </tbody>
-                    </table>
+                    @include('partials.voterpartial')
 
                 </div>
             </div>
