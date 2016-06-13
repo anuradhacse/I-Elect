ec3997fca3c23742668796a84927323be59b2821
Anuradha <www.niroshonmora@gmail.com>
2016-06-13 21:21:15 +0530

    adding candidate partial done

--- resources/views/candidates/create.blade.php
+++ resources/views/candidates/create.blade.php
@@ -53,48 +53,7 @@
                     <hr color="red" />
                     <!-- dispaly CANDIDATES who have already added-->
 
-                    <table id="example" class="display" cellspacing="0" width="100%">
-                        <thead>
-                        <tr>
-                            <th>Candidate Name</th>
-                            <th>Email</th>
-                            <th>Description</th>
-                            <th>Image</th>
-                            <th>Action</th>
-
-                        </tr>
-                        </thead>
-                        <tfoot>
-                        <tr>
-                            <th>Candidate Name</th>
-                            <th>Email</th>
-                            <th>Description</th>
-                            <th>Image</th>
-                            <th>Action</th>
-
-                        </tr>
-                        </tfoot>
-                        <tbody>
-
-                        @foreach($candidates as $candidate)
-                            <tr>
-                                <td>{{$candidate->name}}</td>
-                                <td>{{$candidate->email}}</td>
-                                <td>{{$candidate->description}}</td>
-                                <td><img src="{{asset('uploads/'.$candidate->image_path)}}" style="width: 50px;height: 50px"></td>
-                                <td>{!! Form::open(['method'=>'DELETE','action' => ['CandidateController@delete', $candidate->id]])  !!}
-                                    {!! Form::hidden('election_id',$id) !!}
-                                    <button type="submit" class="btn btn-danger btn-mini">Delete</button>
-                                    {!! Form::close() !!}</td></td>
-
-                            </tr>
-
-                        @endforeach
-
-
-
-                        </tbody>
-                    </table>
+                @include('partials.candidatepartial')
 
                 </div>
             </div>
--- resources/views/partials/candidatepartial.blade.php
+++ resources/views/partials/candidatepartial.blade.php
@@ -1,7 +1,42 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Anuradha
- * Date: 6/13/2016
- * Time: 9:17 PM
- */
+<table id="example" class="display" cellspacing="0" width="100%">
+    <thead>
+    <tr>
+        <th>Candidate Name</th>
+        <th>Email</th>
+        <th>Description</th>
+        <th>Image</th>
+        <th>Action</th>
+
+    </tr>
+    </thead>
+    <tfoot>
+    <tr>
+        <th>Candidate Name</th>
+        <th>Email</th>
+        <th>Description</th>
+        <th>Image</th>
+        <th>Action</th>
+
+    </tr>
+    </tfoot>
+    <tbody>
+
+    @foreach($candidates as $candidate)
+        <tr>
+            <td>{{$candidate->name}}</td>
+            <td>{{$candidate->email}}</td>
+            <td>{{$candidate->description}}</td>
+            <td><img src="{{asset('uploads/'.$candidate->image_path)}}" style="width: 50px;height: 50px"></td>
+            <td>{!! Form::open(['method'=>'DELETE','action' => ['CandidateController@delete', $candidate->id]])  !!}
+                {!! Form::hidden('election_id',$id) !!}
+                <button type="submit" class="btn btn-danger btn-mini">Delete</button>
+                {!! Form::close() !!}</td></td>
+
+        </tr>
+
+    @endforeach
+
+
+
+    </tbody>
+</table>
