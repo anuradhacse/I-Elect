f0c6c0923c15355a2cc52ac11b488d623eaf274d
Anuradha <www.niroshonmora@gmail.com>
2016-06-15 14:18:19 +0530

    delete admin nav patioal unwantad tab

--- resources/views/partials/adminnav.blade.php
+++ resources/views/partials/adminnav.blade.php
@@ -4,12 +4,6 @@
         <li class="active"><a href='{{url('/adminhome')}}'>Elections</a></li>
         <li><a href="#">Settings</a></li>
         <li><a href="{!! url('/elections/create') !!}" >Create New Election</a></li>
-        <li class="dropdown">
-            <a href="#" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-                <li><a href="#tab4info" data-toggle="tab">Info 4</a></li>
-                <li><a href="#tab5info" data-toggle="tab">Info 5</a></li>
-            </ul>
-        </li>
+        
     </ul>
 </div>
